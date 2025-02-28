b41fe0672f8dafab0c5936aee66a5dfd83d4dce8
Benaliyashodhara <kaveesharasandeepa@gmail.com>
2025-02-28 10:39:34 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -3,7 +3,7 @@ const config = require('../config');
 
 cmd({
     pattern: "alive",
-    react: "🤌",
+    react: "👋",
     desc: "Check bot online or no.",
     category: "main",
     filename: __filename
@@ -11,7 +11,7 @@ cmd({
 async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 await robin.sendPresenceUpdate('recording', from);
-await robin.sendMessage(from, { audio: { url: "https://github.com/NethminaPansil/Whtsapp-bot/raw/refs/heads/main/ElevenLabs_2025-02-17T10_20_51_Aria_pre_s50_sb75_se0_b_m2(2).mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
+await robin.sendMessage(from, { audio: { url: "https://github.com/Benaliyashodhara/Kaveeshara-/raw/refs/heads/main/Record-2025-02-21-22-41-27.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
 
 return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
     
