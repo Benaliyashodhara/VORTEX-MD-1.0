ec0b89f2a6fd4c175fc826baa2ad64f239e2475b
Benaliyashodhara <kaveesharasandeepa@gmail.com>
2025-02-28 14:05:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "rzgjkIhL#tzY0Nv24j6v7V80MttRp29Fznm1HJkxX6N1zgP-5ST4",
   OWNER_NUM: process.env.OWNER_NUM || "94781805191",
   PREFIX:process.env.PREFIX || ".",
-  ALIVE_IMG:process.env.ALIVE_IMG||"https://raw.githubusercontent.com/Benaliyashodhara/Kaveeshara-/refs/heads/main/738559.jpg?token=GHSAT0AAAAAAC7BCFEPEJOULXZ7NP2NWMKEZ6BOJWA",
+  ALIVE_IMG:process.env.ALIVE_IMG||"https://raw.githubusercontent.com/NethminaPansil/Whtsapp-bot/refs/heads/main/728202.jpg",
   ALIVE_MSG:process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n Join my Support group using this Link 📥 \n\n https://chat.whatsapp.com/EXPFUXpHxFR5ur03emaNx0 \n\n 💝 𝐌𝐚𝐝𝐞 𝐛𝐲 Kaveeshara 💝 \n\n 🔒 Q҈U҈E҈E҈N҈ B҈E҈N҈A҈L҈I҈ X҈D҈ 🔒 ",
   MODE:process.env.MODE || "public",
   AUTO_VOICE:process.env.AUTO_VOICE|| "true",
